67cefa3b3b8b965dd78a1f259d9ac5a8d611aaec
drhawks68587 <dillon.ross87@gmail.com>
2025-02-11 14:33:02 -0500

    split images and links added

--- src/textnode.py
+++ src/textnode.py
@@ -30,6 +30,62 @@ def extract_markdown_links(text):
     matches = re.findall(pattern, text)
     return [url for _, url in matches]
 
+def split_nodes_image(old_nodes):
+    split_images = []
+    for node in old_nodes:
+        if node.text_type == TextType.NORMAL_TEXT:
+            images = extract_markdown_images(node.text)
+            if images:
+                remaining_text = node.text
+                for url in images:
+                    full_markdown = f"![alt text]({url})"
+
+                    parts = remaining_text.split(full_markdown, 1)
+                    before = parts[0]
+                    remaining_text = parts[1] if len(parts) > 1 else ""
+
+                    if before:
+                        split_images.append(TextNode(before, TextType.NORMAL_TEXT))
+
+                    split_images.append(TextNode("", TextType.IMAGES, url))
+                if remaining_text:
+                    split_images.append(TextNode(remaining_text, TextType.NORMAL_TEXT))
+            else:
+                split_images.append(node)
+        else:
+            split_images.append(node)
+    return split_images
+
+
+
+def split_nodes_link(old_nodes):
+    split_links = []
+    for node in old_nodes:
+        if node.text_type == TextType.NORMAL_TEXT:
+            links = extract_markdown_links(node.text)
+            if links:
+                remaining_text = node.text
+                for link_text, url in links:
+                    full_markdown = f"[{link_text}]({url})"
+
+                    parts = remaining_text.split(full_markdown, 1)
+                    before = parts[0]
+                    remaining_text = parts[1] if len(parts) > 1 else ""
+
+                    if before:
+                        split_links.append(TextNode(before, TextType.NORMAL_TEXT))
+                        split_links.append(TextNode(link_text, TextType.LINK, url))
+
+                if remaining_text:
+                    split_links.append(TextNode(remaining_text, TextType.NORMAL_TEXT))
+            else:
+                split_links.append(node)
+        else:
+            split_links.append(node)
+    return split_links
+
+        
+
 
 
 
