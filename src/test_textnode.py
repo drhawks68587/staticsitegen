a9917da10f351c195b7a8c2e1a697c2c830eb5bf
drhawks68587 <dillon.ross87@gmail.com>
2025-02-10 21:03:11 -0500

    added test cases for links  and images

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from markdown_extractor import extract_markdown_images, extract_markdown_links
 
 
 class TestTextNode(unittest.TestCase):
@@ -31,6 +32,16 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD_TEXT)
         node.type = TextType.ITALIC_TEXT
         self.assertEqual(node.type, TextType.ITALIC_TEXT)
+
+    def test_extract_markdown_images(self):
+        markdown_text = "![alt text](image1.png) ![another image](image2.jpg)"
+        expected_images = ["image1.png", "image2.jpg"]
+        self.assertEqual(extract_markdown_images(markdown_text), expected_images)
+
+    def test_extract_markdown_links(self):
+        markdown_text = "[example](http://example.com) [another link](http://another.com)"
+        expected_links = ["http://example.com", "http://another.com"]
+        self.assertEqual(extract_markdown_links(markdown_text), expected_links)
 
 
 if __name__ == "__main__":
