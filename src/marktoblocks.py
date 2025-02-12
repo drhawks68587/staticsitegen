3e67ede394998c58901b88fedc147303bfbdea78
drhawks68587 <dillon.ross87@gmail.com>
2025-02-11 19:16:46 -0500

    block to block type added

--- src/marktoblocks.py
+++ src/marktoblocks.py
@@ -1,5 +1,6 @@
 from htmlnode import *
 from textnode import *
+import re
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -9,3 +10,19 @@ def markdown_to_blocks(markdown):
             stripped_blocks.append(block.strip())
     return stripped_blocks
 
+def block_to_block_type(block):
+    lines = block.split('\n')
+
+    if re.match(r'#{1,6} \S', block):
+        return "heading"
+    if block.startswith("```") and block.endswith("```"):
+        return "code"
+    if all(line.startswith(">") for line in lines):
+        return "quote"
+    if all(re.match(r'^[*-] ', line) for line in lines):
+        return "unordered_list"
+    if all(re.match(r'\d+\. \S', line) for line in lines):
+        numbers = [int(re.match(r'(\d+)\. ', line).group(1)) for line in lines]
+        if numbers == list(range(1, len(numbers) + 1)):
+            return "ordered_list"
+    return "paragraph"
